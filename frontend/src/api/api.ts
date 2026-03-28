e8665514e699a68b5545df3d39f000c5fc08ac7e
Ismail M <muhammedismailkt@gmail.com>
2026-03-28 20:48:05 +0530

    frontende

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log(API_URL,'API_URL');
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${API_URL}/api`,
   withCredentials: true, // For handling HTTP-only cookies like refreshToken
 });
 
